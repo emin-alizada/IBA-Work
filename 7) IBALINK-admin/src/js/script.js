c2f606188a0b83d711af0517504b98440475a2ec
Emin <emin.134.134@gmail.com>
2020-05-31 09:12:47 +0400

    latest changes before work

---
+++ 7) IBALINK-admin/src/js/script.js
@@ -0,0 +1,46 @@
+$('#otpDesc').find('input').each(function() {
+    $(this).attr('maxlength', 1);
+
+    $(this).on('keyup', function(e) {
+        var parent = $($(this).parent());
+        if(e.keyCode === 8 || e.keyCode === 37) {
+            var prev = parent.find('input#' + $(this).data('previous'));
+
+            if(prev.length) {
+                $(prev).select();
+            }
+        } else if((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode === 39) {
+            var next = parent.find('input#' + $(this).data('next'));
+
+            if(next.length) {
+                $(next).select();
+            } else {
+                if(parent.data('autosubmit')) {
+                    parent.submit();
+                }
+            }
+        }
+    });
+});
+
+function countdown(seconds, element) {
+    let minutesDisplay, secondsDisplay;
+    setInterval(calculate, 1000);
+
+    function calculate() {
+        if ( seconds >= 0 ) {
+            minutesDisplay = parseInt(seconds / 60);
+            secondsDisplay = parseInt(seconds % 60);
+
+            $("#otpTimer").text(`${("0" + minutesDisplay).slice(-2)}:${("0" + secondsDisplay).slice(-2)}`);
+            seconds--;
+        } else {
+            clearInterval(this);
+            // do the work here (end of timer)
+        }
+    }
+}
+
+if ($("#otpTimer")){
+    countdown(120, $("#otpTimer"));
+}

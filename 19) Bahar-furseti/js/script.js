909f1df08c8852b5b7a966224404f57e8284973b
Emin Alizada <emin.134.134@gmail.com>
2021-03-09 11:01:58 +0400

    bahar furseti initial

---
+++ 19) Bahar-furseti/js/script.js
@@ -0,0 +1,22 @@
+function openModal(e, modalId) {
+    document.body.style.overflow = "hidden";
+    const id = modalId || $(this).data("modalId");
+    $(`#${id}`).fadeIn("fast");
+
+    $(`#${id}`).on('click', function (event) {
+        if (id === event.target.id) {
+            closeModal();
+        }
+    })
+}
+
+function closeModal() {
+    $(".modal-container").fadeOut("fast");
+    document.body.style.overflow = "auto";
+}
+
+$('.card').on('click', openModal);
+
+$(document).ready(function (e) {
+    $('.card').addClass('animated');
+});
